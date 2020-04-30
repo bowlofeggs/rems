8f754a14de85bea754927260ebfe3eb924c5e892
Randy Barlow <randy@electronsweatshop.com>
2020-04-30 00:35:15 -0400

    Add a format check to devel/test.sh
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/test.sh
+++ devel/test.sh
@@ -5,4 +5,4 @@ PARAMS=$@
 
 podman run --rm -it -v $SRC_DIR:/devel:z \
 	-e RUST_BACKTRACE=1 rems:dev \
-	bash -c "cd /devel && cargo test && cargo clippy --all-targets --all-features -- -D warnings && cargo doc"
+	bash -c "cd /devel && cargo test && cargo clippy --all-targets --all-features -- -D warnings && cargo doc && cargo fmt -- --check -v"
