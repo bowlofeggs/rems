3bf2b604995ea760b5634c832cb67fcd514cf366
Randy Barlow <randy@electronsweatshop.com>
2020-05-14 20:43:51 -0400

    Allow test.sh to use network
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/test.sh
+++ devel/test.sh
@@ -3,6 +3,6 @@
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 PARAMS=$@
 
-podman run --rm -it -v $SRC_DIR:/devel:z \
+podman run --network=host --rm -it -v $SRC_DIR:/devel:z \
 	-e RUST_BACKTRACE=1 rems:dev \
 	bash -c "cd /devel && cargo test && cargo clippy --all-targets --all-features -- -D warnings && cargo doc && cargo fmt -- --check -v"
